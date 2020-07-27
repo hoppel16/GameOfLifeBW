852fd9fc18715f03d79f7d5addd8199797fd3682
Hunter Oppel <hunter.oppel@gmail.com>
2020-07-27 12:31:10 -0400

    Added grid model

--- GameOfLife/GameOfLife/Models/Grid.swift
+++ GameOfLife/GameOfLife/Models/Grid.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+class Grid {
+    var cells: [Cell]
+    let dimensions: Int = 20
+
+    init() {
+        cells = [Cell]()
+
+        for x in 0..<dimensions {
+            for y in 0..<dimensions {
+                cells.append(Cell(x: x, y: y))
+            }
+        }
+    }
+
+    subscript(x: Int, y: Int) -> Cell? {
+        return cells.filter { $0.x == x && $0.y == y }.first
+    }
+}
